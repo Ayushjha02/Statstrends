61e7108bb8d42e73b946362e4cee10dd4b21884b
Ayushjha02 <117091650+Ayushjha02@users.noreply.github.com>
2022-12-07 17:11:46 +0000

    New changes added
    Added new formatting changes and scatter graph

--- Stats_Trends.py
+++ Stats_Trends.py
@@ -1,20 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec  1 15:27:49 2022
-@author: Aayush Jha
+Created on Thu Dec  1 15:27:49 2022.
+
+@author: Ayush Jha
+@Student Id: 21063203
 Assignment : Statistics and trend
 """
 # Imports
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.stats as sci
 
 
 # Function to read and transpose the file
 def read_Data(fileName):
-    """This Function read the data from the csv file,Clean the data to make
-    it usable and transpose the data,transposed data further get cleaned to
-    include as needed"""
+    """
+    Read,Clean and Transpose.
+
+    function read the data from the csv file,Clean the data to make
+    it usable and transpose the data.transposed data further get cleaned to
+    include header as needed
+    """
     df_Data1 = pd.read_csv(fileName)
 # using trimData function to remove unwanted data
     df_Data = trimData(df_Data1)
@@ -25,8 +32,12 @@ def read_Data(fileName):
 
 # Function to  clean the transposed data
 def cleanDataTrans(dataframe):
-    """This Function cleans the transposed data by removing unwanted header
-    and change it to use country name and use year as index"""
+    """
+    Clean the transposed data.
+
+    function clean the data by removing unwanted header
+    and change it to use country name and use year as index
+    """
     dataframe.rename(columns=dataframe.loc["Country Name"], inplace=True)
     dataframe = dataframe.drop(["Country Name"], axis=0)
     dataframe.index.name = "Year"
@@ -35,18 +46,19 @@ def cleanDataTrans(dataframe):
 
 # Function to trim the data in a good format
 def trimData(DatasetName):
-    """This function removes the unwanted data from the dataframe and return
-    only usable countries and indicators"""
+    """
+    Remove the unwanted data.
+
+    return only usable countries and indicators
+    """
     Countries = ['China', 'India', 'United States', 'United Kingdom',
                  'Germany',
                  'Brazil', 'Australia', 'South Africa']
 
     Indicators = ['CO2 emissions (kt)',
-                  'Methane emissions (kt of CO2 equivalent)',
-                  'Nitrous oxide emissions (thousand metric tons of CO2 equivalent)',
                   'Nitrous oxide emissions (% change from 1990)',
                   'Methane emissions (% change from 1990)',
-                  'Population, total', 'Electricity production from renewable sources, excluding hydroelectric (% of total)',
+                  'Population, total',
                   'Electricity production from coal sources (% of total)',
                   'Forest area (sq. km)']
 
@@ -63,8 +75,7 @@ def trimData(DatasetName):
 
 # Function to fetch data besad on data indicator
 def dataIndicator(Name):
-    """This Function return the data besad on the indicator name
-    passed"""
+    """Return the data besad on the indicator name."""
     Dataframe = df_climateChange.loc[df_climateChange['Indicator Name']
                                      == Name]
     Dataframe = Dataframe.transpose()
@@ -77,9 +88,13 @@ def dataIndicator(Name):
 
 # Function used for bar graph
 def barPlotDataManp():
-    """This Function manupulates the data used to plot bar graphs"""
+    """
+    Manupulates the data for appropriate graph.
+
+    Function used to get all the parameters require to plot bar graphs
+    """
     Dataframe = dataIndicator(
-       'Electricity production from coal sources (% of total)')
+        'Electricity production from coal sources (% of total)')
     Dataframe = Dataframe.iloc[:-6]
     Dataframe = Dataframe[::6]
     return Dataframe
@@ -87,7 +102,11 @@ def barPlotDataManp():
 
 # Function used for Pie graph
 def pieChartdatManu():
-    """This function manupulates the data used to Pie Chart graphs"""
+    """
+    Manupulates the data for appropriate graph.
+
+    Function used to fetch all the data for Pie Chart graphs.
+    """
     Dataframe = dataIndicator('Population, total')
     Dataframe = Dataframe.iloc[[-1]].iloc[0:1, :-1]
     myLabels = np.array(Dataframe.columns)
@@ -98,16 +117,26 @@ def pieChartdatManu():
 
 # Function used for scatter plot
 def Scatterdatamanu():
+    """
+    Manupulates the data for appropriate graph.
+
+    function uses mean method to get the mean for all indicator grouped
+    by and then return the data useful to plot scatter
+    """
     Dataframe = df_climateChange.groupby(by=['Indicator Name']).mean()
-    nitrous_change = np.array(Dataframe.iloc[6, 2:24])
-    methane_change = np.array(Dataframe.iloc[4, 2:24])
+    nitrous_change = np.array(Dataframe.iloc[4, 2:24])
+    methane_change = np.array(Dataframe.iloc[3, 2:24])
     yearSeries = np.linspace(1991, 2012, 22)
     return Dataframe, nitrous_change, methane_change, yearSeries
 
 
 # Function used for plot
 def plotdatamanu():
-    """This function manupulates the data used to plot"""
+    """
+    Manupulates the data for appropriate graph.
+
+    Function to manupulate the data used to plot graph
+    """
     Dataframe = dataIndicator('CO2 emissions (kt)')
     Dataframe = Dataframe.iloc[:-2].iloc[1:-1, 0:9]
     Dataframe = Dataframe[::3]
@@ -118,13 +147,13 @@ def plotdatamanu():
 df_climateChange, df_climateChange_trans = read_Data('Data.csv')
 
 
-# Barplot
-# Using bar plot method to get all the required data for bar plot
+# Barplot(using pandas dataframe method)
+# Using pandas bar plot method to get all the required data for bar plot
 df_elec = barPlotDataManp()
 ax = df_elec.plot.bar(width=0.8)
 title = 'coal Consumption in different country for electricity production(%)'
 ax.plot(rot=45, title=title)
-ax.legend(loc='center left', bbox_to_anchor=(1.0, .7))
+ax.legend(loc='center left', bbox_to_anchor=(1.0, 0.7))
 plt.ylabel("Percentage of coal consumption")
 plt.show()
 
@@ -143,7 +172,7 @@ plt.plot(df_co2["Year"], df_co2["South Africa"], label="South Africa")
 plt.plot(df_co2["Year"], df_co2["Germany"], label="Germany")
 plt.xlabel("Year")
 plt.ylabel("CO2 Consumption")
-plt.title("C02 consumption by countries for period of 1989-2017")
+plt.title("CO2 emissions (kt) by countries for period of 1989-2017")
 plt.legend(loc=2, prop={'size': 7})
 plt.show()
 
@@ -161,13 +190,14 @@ plt.show()
 # Using Scatterdatamanu function which returns data useful for scatters
 Dataframe, nitrous_change, methane_change, yearSeries = Scatterdatamanu()
 plt.figure()
-plt.scatter(yearSeries, nitrous_change, label='new',
-            alpha=0.3, edgecolors='none')
-plt.scatter(yearSeries, methane_change, label='old',
+plt.scatter(yearSeries, nitrous_change,
+            alpha=0.5, edgecolors='none')
+plt.scatter(yearSeries, methane_change,
             alpha=0.9, edgecolors='none')
-plt.title("Mean of changes in use of nitrous oxide and methane over the period of 1991-2012(%)")
-plt.ylabel('Change in Percentage')
-plt.xlabel('Year')
+plt.title('Mean of changes in emissions of nitrous oxide and methane over the'
+          ' period of 1991-2012(%)')
+plt.ylabel('Change in Percentage -->')
+plt.xlabel('Year -->')
 plt.legend(
     ('Nitrous Oxide', 'Methane'),
     scatterpoints=1,
@@ -175,3 +205,8 @@ plt.legend(
     ncol=3,
     fontsize=8)
 plt.show()
+
+
+# heatmap
+df_frame = df_climateChange[df_climateChange['Country Name']=='China']
+df_frame = df_frame.iloc[:,4:26]
